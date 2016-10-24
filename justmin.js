89ce168b22e882763970dd1ae377c26821db880a
Nick <xpxt@outlook.com>
2016-10-25 00:27:06 +0400

    1

--- justmin.js
+++ justmin.js
@@ -1,1 +1,75 @@
-window.console.log ('ok');
+var window = window;
+	window.tick = 50;
+	window.time = 0;
+
+	window.events = {
+		set load (update) {
+			window.onmousedown = update;
+			window.onmousemove = update;
+			window.onmouseup = update;
+			window.on.tick = update;
+		}
+	}
+
+	window.load = function (update) {
+		window.events.load = update;
+	}
+
+	window.on = {
+		set tick (update) {
+			window.setInterval (function () {
+				window.time += window.tick;
+				update ({type: 'tick'});
+			}, window.tick);
+		}
+	}
+
+var canvas = window.document.createElement ('canvas');
+
+	canvas.css = function () {
+		canvas.style.left = 0;
+		canvas.style.position = 'absolute';
+		canvas.style.top = 0;
+	}
+
+	canvas.load = function () {
+		canvas.css ();
+		canvas.resize ();
+		window.document.body.appendChild (canvas);
+		window.onresize = canvas.resize;
+	}
+
+	canvas.resize = function () {
+		canvas.height = window.innerHeight;
+		canvas.width = window.innerWidth;
+	}
+
+var context = canvas.getContext ('2d');
+
+var app = {
+	load: function () {
+		window.load (app.update);
+		canvas.load ();
+	},
+
+	object: {
+		test: {
+			mousedown: function () {
+				window.console.log ('test');
+			}
+		}
+	},
+
+	update: function (event)
+	{
+		for (let id in app.object)
+		{
+			for (let method in app.object[id])
+			{
+				if (method == event.type) { app.object[id][method] (event); }
+			}
+		}
+	}
+}
+
+window.onload = app.load;
